5b6b59ffeafcd118471be0a5c6f9642ac268450a
unicode6674 <18837028037@163.com>
2019-12-29 21:20:12 +0800

    sass

--- src/screens/PageOne/PageOne.js
+++ src/screens/PageOne/PageOne.js
@@ -1,5 +1,5 @@
 import React from 'react';
-require('./PageOne.css');
+require('./PageOne.scss');
 
 class PageOne extends React.Component{
     // 页面1 是用来练习样式的
@@ -18,7 +18,10 @@ class PageOne extends React.Component{
                 <div>PageOne</div>
                 <div style={{ width:'400px', height:'80px', backgroundColor:'yellow', fontSize:'24px', textAlign:'center', color: 'blue' }}>引入样式的方法1</div>
                 <div style={mystyle}>引入样式的方法2</div>
-                <div className={'my-style'}>引入样式的方法3</div>
+                <div className={'my-style'}>
+                    <span>引入样式的方法3</span>
+                    <span className={'child'}>scss测试</span>
+                </div>
             </div>
         );
     }
