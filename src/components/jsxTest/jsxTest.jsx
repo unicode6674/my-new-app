f8f0dc8beb7165113a06fa5a6e7d09e4ab7f6737
unicode6674 <18837028037@163.com>
2019-12-29 14:07:13 +0800

    切换项目

--- src/components/jsxTest/jsxTest.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-var myDivElement = <div className="foo" />;
-render(myDivElement, document.getElementById('mountNode'));
--- src/store/actionTypes.js
+++
@@ -1,2 +0,0 @@
-export const ADD_COUNT = 'ADD_COUNT';
-export const REDUCE_COUNT = 'REDUCE_COUNT';
