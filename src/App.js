fa9314440efef6a01d6310ddae258cfbfe7f36a4
unicode6674 <18837028037@163.com>
2019-12-31 17:07:20 +0800

    react属性的双向绑定和方法绑定this的方式

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router,Route} from 'react-router-dom';
+import { HashRouter as Router,Route} from 'react-router-dom';
 import './App.scss';
+import Nav from './components/Nav';
 
-// 引入导航Nav
-import Nav from './components/Nav'
 // 引入各个页面
 import Home from './../src/screens/Home/Home'
 import PageOne from './../src/screens/PageOne/PageOne'
@@ -25,7 +24,7 @@ class App extends Component {
   /*虚拟dom */
   render() {
     return (
-        <Router >
+        <Router>
             <Nav></Nav>
             <div id={'content'}>
                 <Route exact path="/" component={Home} />
--- src/screens/PageOne/PageOne.js
+++ src/screens/PageOne/PageOne.js
@@ -3,6 +3,15 @@ require('./PageOne.scss');
 
 class PageOne extends React.Component{
     // 页面1 是用来练习样式的
+    constructor(props){
+        super(props);     //继承关键字
+        this.state={
+            value:123 ,   //设置默认值
+        };
+        this.reduction = this.reduction.bind(this);
+    };
+
+
     render(){
         // 引入样式的方法2
         let mystyle={
@@ -23,9 +32,37 @@ class PageOne extends React.Component{
                     <span>引入样式的方法3</span>
                     <span className={'child'}>scss测试</span>
                 </div>
+                <div>
+                    <div>属性的双向绑定</div>
+                    <input value={this.state.value}  type={'number'} onChange={(event)=>{this.change(event)}}/>
+                    <p>{this.state.value}</p>
+                    <div style={{cursor:"pointer"}} onClick={this.dianJi}>点击一下value＋1</div>
+                    <div style={{cursor:"pointer"}} onClick={this.reduction}>点击一下value - 1</div>
+                    <div style={{cursor:"pointer"}} onClick={this.reduction5.bind(this)}>点击一下value - 5</div>
+                </div>
             </div>
         );
     }
+    reduction5() {
+        this.setState({value: Number(this.state.value) - 5})
+    }
+
+    change(e) {
+        console.log(e.target.value);    //获取修改后的值
+        this.setState({
+            value: e.target.value
+        })
+    };
+
+    dianJi = () => {
+       this.setState({value: Number(this.state.value) + 1})
+    };
+
+    reduction() {
+        this.setState({value: Number(this.state.value) - 1})
+    };
+
+
 }
 
 export default PageOne;
